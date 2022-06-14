fdaeb9a5ba9c4c43f45a265397171f3a655354d2
Gi7D-02 <Gi7D-02@Gi7D-11>
2022-06-14 14:50:26 +0900

    Apple에 Float d를 추가

--- src/practice/Apple.java
+++ src/practice/Apple.java
@@ -5,10 +5,13 @@ public class Apple {
 		int a;
 		int b;
 		int c;
+		float d;
 		
 		public Apple(int a, int b, int c ) {
 			this.a = a;
 			this.b = b;
 			this.c = c;
+			
+			this.d = 0.123F;
 		}
 }
